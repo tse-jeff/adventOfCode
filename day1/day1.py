7eaa23dac457741c3a63aa72b75eb33bc19235a4
tse-jeff <jeff.tse@nyu.edu>
2023-12-05 18:10:05 -0500

    finished second part of first day

--- day1/day1.py
+++ day1/day1.py
@@ -1,21 +1,62 @@
-
-
 def sum_of_two_digit_numbers(text):
+    words = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+             "six": 6, "seven": 7, "eight": 8, "nine": 9}
+
+    sum_of_numbers = 0
+
     lines = text.strip().split("\n")
-    numbers = []
     for line in lines:
-        for char in line:
-            if char.isdigit():
-                first_digit = int(char)
-                break
-        for char in reversed(line):
-            if char.isdigit():
-                last_digit = int(char)
-                break
-        numbers.append(first_digit * 10 + last_digit)
+        first_digit = None
+        second_digit = None
 
-    sum_of_numbers = sum(numbers)
+        for i in range(len(line)):
+            if (first_digit):
+                break
 
+            if line[i].isdigit():
+                first_digit = int(line[i])
+                break
+            elif i <= len(line) - 3:
+                word = line[i:i + 3]
+                # print("first word", word)
+                if word in words:
+                    first_digit = words[word]
+                    break
+                j = i + 3
+                while j < len(line) and line[j].isalpha() and len(word) <= 5:
+                    word += line[j]
+                    # print("first word", word)
+                    if word in words:
+                        first_digit = words[word]
+                        break
+                    j += 1
+
+        for i in range(len(line) - 1, -1, -1):
+            if (second_digit):
+                break
+            if line[i].isdigit():
+                second_digit = int(line[i])
+                break
+            elif i >= 2:
+                word = line[i - 2:i + 1]
+                # print("second word", word)
+                if word in words:
+                    second_digit = words[word]
+                    break
+                j = i - 3
+                while j >= 0 and line[j].isalpha() and len(word) <= 5:
+                    word = line[j] + word
+                    # print("second word", word)
+                    if word in words:
+                        second_digit = words[word]
+                        break
+                    j -= 1
+
+        num = first_digit * 10 + second_digit
+        # print(line, num)
+        # print()
+
+        sum_of_numbers += num
     return sum_of_numbers
 
 
